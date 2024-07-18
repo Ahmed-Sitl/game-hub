c0d277bfce8176d36fc3730e153e670085c4d746
Ahmed Nabil <bplahmed28d@gmail.com>
2024-07-18 15:54:54 +0300

    Ship geners with apps

--- src/hooks/useGeners.ts
+++ src/hooks/useGeners.ts
@@ -1,11 +1,9 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-
-const useGeners = () => useData<Genre>("/genres");
+const useGeners = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGeners;
